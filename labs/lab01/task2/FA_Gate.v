9403a363c0f8c470c51987f505dfeff15be54bf5
Shubh Gupta <f20250483@pilani.bits-pilani.ac.in>
2026-08-29 14:51:09 +0000

    lab01 task2

--- labs/lab01/task2/FA_Gate.v
+++ labs/lab01/task2/FA_Gate.v
@@ -5,7 +5,7 @@
 // from here on, not a special add-on.
 //
 // Part (a): add a CONSTANT delay to every gate below, e.g.:
-//   xor #(2) (ps, a, b);
+//   xor #(2,3) (ps, a, b);
 // Do this for all five gates, then complete ripple_adder.v (this folder)
 // using this full adder, and simulate against tb.v.
 //
@@ -26,10 +26,10 @@ module FA_Gate(
 );
   wire ps, pc1, pc2;
 
-  xor (ps,  a,   b);
-  and (pc1, a,   b);
-  xor (sum, cin, ps);
-  and (pc2, cin, ps);
-  or  (cout, pc1, pc2);
+  xor #(2,3) (ps,  a,   b);
+  and #(2,3) (pc1, a,   b);
+  xor #(2,3) (sum, cin, ps);
+  and #(2,3) (pc2, cin, ps);
+  or  #(2,3) (cout, pc1, pc2);
 
 endmodule
