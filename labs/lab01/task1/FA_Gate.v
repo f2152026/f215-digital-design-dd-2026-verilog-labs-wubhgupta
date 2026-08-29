145607a63f837847b95ae549b05b6d46dedfe5cd
Shubh Gupta <f20250483@pilani.bits-pilani.ac.in>
2026-08-29 15:48:15 +0000

    lab01 task1

--- labs/lab01/task1/FA_Gate.v
+++ labs/lab01/task1/FA_Gate.v
@@ -14,13 +14,15 @@ module FA_Gate(
   output sum,
   output cout
 );
+
   wire ps, pc1, pc2;
 
-  or #(2) (cout, pc1, pc2);
   xor #(2) (ps,  a,   b);
   and #(2) (pc1, a,   b);
   xor #(2) (sum, cin, ps);
   and #(2) (pc2, cin, ps);
+  or #(2) (cout, pc1, pc2);
+
 
 
 endmodule
